391c4604aec03ca84beb3367d0e42896ff7fbdc9
ITSECT3R <ITSECT3R@GMAIL.COM>
2025-03-18 19:20:10 -0600

    added JS functionalities to project section and re-structured the project for better scaleability for future projects. also added multiple projects and reduced html file size.

---
+++ projects-data.js
@@ -0,0 +1,79 @@
+import { svgs } from './svgs.js';
+
+export const personalProjects = [
+    {
+    projectID: '2',
+    projectName: 'Ecommerce "Artes Sacro"',
+    projectTechnologies: [svgs.html, svgs.css, svgs.js],
+    projectImage: 'Picture-2-portfolio.png',
+    projectLink: 'https://itsect3r.github.io/E-commerce-Artes-Sacro/',    
+},{
+    projectID: '1', 
+    projectName: '"Snake Game"',
+    projectTechnologies: [svgs.html, svgs.css, svgs.js],
+    projectImage: 'Picture-1-portfolio.png',
+    projectLink: 'https://itsect3r.github.io/Snake-ITSECT3R/',
+}]  // Load first set of projects;
+
+export const freeCodeCampProjects = [
+{
+    projectID: '9',
+    projectName: 'Pokemon Search App',
+    projectTechnologies: [svgs.html, svgs.css, svgs.js],
+    projectImage: 'Picture-9-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Pokemon-Search-app/',
+},
+{
+    projectID: '8',
+    projectName: 'Cash Register',
+    projectTechnologies: [svgs.html, svgs.css, svgs.js],
+    projectImage: 'Picture-8-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Cash-Register/',
+},
+{
+    projectID: '7',
+    projectName: 'Telephone Number Validator',
+    projectTechnologies: [svgs.html, svgs.css, svgs.js],
+    projectImage: 'Picture-7-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Telephone-Number-Validator/',
+},
+{
+    projectID: '6',
+    projectName: 'Roman Numeral Converter',
+    projectTechnologies: [svgs.html, svgs.css, svgs.js],
+    projectImage: 'Picture-6-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Roman-Numeral-Converter/',
+},
+{
+    projectID: '5',
+    projectName: 'Palindrome Checker',
+    projectTechnologies: [svgs.html, svgs.css, svgs.js],
+    projectImage: 'Picture-5-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Palindrome-Checker/',
+}, 
+{
+    projectID: '4', 
+    projectName: '"Knights" PC Ecommerce Page',
+    projectTechnologies: [svgs.html, svgs.css],
+    projectImage: 'Picture-4-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Product-Landing-Page-FreeCodeCamp/',
+},{
+    projectID: '3', 
+    projectName: 'Learn How to build your PC',
+    projectTechnologies: [svgs.html, svgs.css],
+    projectImage: 'Picture-3-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Technical-Documentation-Page-FreeCodeCamp-/',
+},{
+    projectID: '2', 
+    projectName: 'Tribute Page',
+    projectTechnologies: [svgs.html, svgs.css],
+    projectImage: 'Picture-2-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Tribute-Page-FreeCodeCamp/',
+},{
+    projectID: '1', 
+    projectName: 'Survey Form',
+    projectTechnologies: [svgs.html, svgs.css],
+    projectImage: 'Picture-1-freeCodeCamp.png',
+    projectLink: 'https://itsect3r.github.io/Survey-form.-FreeCodeCamp-/',
+}];
+// Load first set of projects

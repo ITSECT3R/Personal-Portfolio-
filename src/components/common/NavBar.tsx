82c71b23e8a511ffb57d1cad91ea98b4b3c97846
Itsect3r <itsect3r@gmail.com>
2026-01-09 23:38:21 -0600

    add NavBar component with styled buttons and icons; update dependencies for lucide-react and react-icons

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom"
+import { Home, Briefcase, FolderGit2, Code } from "lucide-react"
+import './NavBar.css'
 
 export default function NavBar() {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/experience">Experience</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/skills">Skills</Link>
+      <Link to="/" className="nav-button">
+        <Home className="nav-icon" />
+        <span>Home</span>
+      </Link>
+      <Link to="/experience" className="nav-button">
+        <Briefcase className="nav-icon" />
+        <span>Experience</span>
+      </Link>
+      <Link to="/projects" className="nav-button">
+        <FolderGit2 className="nav-icon" />
+        <span>Projects</span>
+      </Link>
+      <Link to="/skills" className="nav-button">
+        <Code className="nav-icon" />
+        <span>Skills</span>
+      </Link>
     </nav>
   )
 }

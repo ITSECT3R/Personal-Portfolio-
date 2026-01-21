e1f89ecccb978613f97e2b126817b6969400242e
Itsect3r <itsect3r@gmail.com>
2026-01-20 23:47:53 -0600

    feat: update Home component with a new description section and enhance CSS styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,17 @@ export default function Home() {
         Full-Stack Developer
       </h1>
       <div className="content-wrapper">
-        <div></div>
+        <div className="description-container">
+          <p>
+            Hello! I am a passionate Full-Stack Software Developer dedicated to
+            continuously grow in this dynamic field. I have actively built and
+            contributed to various innovative solutions and applications,
+            consistently striving to deliver high-quality and impactful results.
+            My focus is on leveraging my skills to create cutting-edge software
+            and further my expertise in this exciting field. Thank you for
+            visiting!
+          </p>
+        </div>
         <div>
           <div className="img-container  border-effect border-dual-spin border-thick border-hover-only">
             <img
